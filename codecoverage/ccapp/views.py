4088addaf8c163b46c89ff2aa78c69d1c8a06c3d
Lukas Marschall <lmarschall.com@gmail.com>
2020-08-07 19:28:09 +0200

    first iteration of website with basic functions

--- codecoverage/ccapp/views.py
+++ codecoverage/ccapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+
+    context = {'search_string': ""}
+    return render(request, 'ccapp/index.html', context)
+
+def checkUrl(request):
+
+    query = request.GET.get('search_string')
+    context = {'search_string': query}
+    # search_string = url_string
+    return render(request, 'ccapp/index.html', context)
---
+++ codecoverage/ressources/ccapp/js/app.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: '#searchframe',
+    delimiters: ['${','}'],
+    data:
+    {
+        // position: { lat: 50.775470, lng: 6.083811 },
+        // allowedBounds: null,
+        // schnors: [],
+        // markers: [],
+        // ready: false,
+        // map: null,
+    },
+    mounted: function () {
+        // this.getSchnors();
+    },
+    methods:
+    {
+        checkUrl: function()
+        {
+        }
+    }
+});
